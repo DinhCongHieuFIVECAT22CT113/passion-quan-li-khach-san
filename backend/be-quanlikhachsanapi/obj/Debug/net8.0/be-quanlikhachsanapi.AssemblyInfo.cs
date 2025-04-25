ac926d58651e1d1c52a982b05a89e3708b0b2285
DinhCongHieu_FIVECAT_22CT113 <163360363+DinhCongHieuFIVECAT22CT113@users.noreply.github.com>
2025-04-26 06:25:02 +0700

    update loại bỏ conect hard

--- backend/be-quanlikhachsanapi/obj/Debug/net8.0/be-quanlikhachsanapi.AssemblyInfo.cs
+++ backend/be-quanlikhachsanapi/obj/Debug/net8.0/be-quanlikhachsanapi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("be-quanlikhachsanapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f485aca445ec201205bf111f30f6ec4f6e9bd11")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c4205be2eef9a6298fd8dddf49f95aa97595b91")]
 [assembly: System.Reflection.AssemblyProductAttribute("be-quanlikhachsanapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("be-quanlikhachsanapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
