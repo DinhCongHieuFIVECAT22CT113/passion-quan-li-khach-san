84e560e6a4cf172fb8710d8d9a9bba4ad72bdee6
LeChiTuong_FIVECAT <lct8104@gmail.com>
2025-05-06 12:43:00 +0700

    API check lịch làm việc của nhân viên

--- backend/be-quanlikhachsanapi/obj/Debug/net8.0/be-quanlikhachsanapi.AssemblyInfo.cs
+++ backend/be-quanlikhachsanapi/obj/Debug/net8.0/be-quanlikhachsanapi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("be-quanlikhachsanapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9cb4c7e765e0f6fede53f7cd66fddc8df31174d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+629cb7fc75a9c2fc7ac898dc5c921804c030b767")]
 [assembly: System.Reflection.AssemblyProductAttribute("be-quanlikhachsanapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("be-quanlikhachsanapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
