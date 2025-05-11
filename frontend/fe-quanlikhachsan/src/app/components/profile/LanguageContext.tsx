052cda3037dece7991809ac68fbaef4f6b6b1800
TruongTrungTin_FIVE_CAT <128278161+TruongTrungTinFIVECAT22CT113@users.noreply.github.com>
2025-05-11 18:33:30 +0700

    thêm tiếng Hàn/Nhật/Trung/Thái/Anh/Đức/Pháp/Nga

--- frontend/fe-quanlikhachsan/src/app/components/profile/LanguageContext.tsx
+++ frontend/fe-quanlikhachsan/src/app/components/profile/LanguageContext.tsx
@@ -20,14 +20,14 @@ const LanguageContext = createContext<LanguageContextType | undefined>(undefined
 export const LanguageProvider = ({ children }: { children: ReactNode }) => {
   const [languages, setLanguages] = useState<Language[]>([
   { id: 1, name: 'Tiếng Việt', code: 'vi', status: 'Đang sử dụng' },
-  { id: 2, name: '한국어', code: 'ko', status: 'Đang sử dụng' }, // Hàn
+  { id: 2, name: '한국어', code: 'ko', status: 'Đang sử dụng' }, // Tiếng Hàn
   { id: 3, name: '日本語', code: 'ja', status: 'Đang sử dụng' }, // Nhật
   { id: 4, name: '中文', code: 'zh', status: 'Đang sử dụng' }, // Trung
   { id: 5, name: 'ภาษาไทย', code: 'th', status: 'Đang sử dụng' }, // Thái
   { id: 6, name: 'English', code: 'en', status: 'Đang sử dụng' }, // Anh 
   { id: 7, name: 'Deutsch', code: 'de', status: 'Đang sử dụng' }, // Đức
   { id: 8, name: 'Français', code: 'fr', status: 'Đang sử dụng' }, // Pháp
-  { id: 9, name: 'Русский', code: 'ru', status: 'Đang sử dụng' }, // Nga
+  { id: 9, name: 'Русский', code: 'ru', status: 'Đang sử dụng' }, // Tiếng Nga
   ]);
 
   const [selectedLanguage, setSelectedLanguage] = useState<string>('vi');
