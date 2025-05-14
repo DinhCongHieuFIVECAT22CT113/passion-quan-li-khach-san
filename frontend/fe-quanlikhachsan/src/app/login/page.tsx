5da338fc648c0ee9e994798f9b0fc27fcd68bf85
Phùng Mạnh Dũng <mdung07102004@gmai.com>
2025-05-14 20:52:46 +0700

    sửa đăng nhập đăng kí nè

--- frontend/fe-quanlikhachsan/src/app/login/page.tsx
+++ frontend/fe-quanlikhachsan/src/app/login/page.tsx
@@ -1,12 +1,18 @@
 'use client';
 import React from 'react';
 import LoginForm from '../../app/components/logsign/LoginForm';
+import './page.css';
 
 const LoginPage: React.FC = () => {
   return (
-    <div className="container">
-      <LoginForm />
-      <div className="image-section" />
+    <div className="login-container">
+      <div className="form-wrapper">
+        <LoginForm />
+      </div>
+      <div className="image-section">
+        <h2>Chào mừng đến với Passion Horizon</h2>
+        <p>Trải nghiệm quản lý khách sạn chuyên nghiệp và tiện lợi nhất.</p>
+      </div>
     </div>
   );
 };
