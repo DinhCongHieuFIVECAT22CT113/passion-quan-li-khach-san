ff8ba36020cbdf8c236a128fd09bcbe2ccb91e64
LeChiTuong_FIVECAT <lct8104@gmail.com>
2025-05-18 14:07:10 +0700

    Update thêm đổi mật khẩu cho nhân viên

--- backend/be-quanlikhachsanapi/DTOs/KhachHangDto.cs
+++ backend/be-quanlikhachsanapi/DTOs/KhachHangDto.cs
@@ -38,15 +38,15 @@ namespace be_quanlikhachsanapi.DTOs
         public string SoCccd { get; set; } = null!;
         public string? MaLoaiKh { get; set; }
     }
-    public class ChangePassDto
-    {
-        [Required]
-        public required string Password { get; set; }
+    // public class ChangePassDto
+    // {
+    //     [Required]
+    //     public required string Password { get; set; }
 
-        [Required]
-        public required string NewPassword { get; set; }
+    //     [Required]
+    //     public required string NewPassword { get; set; }
 
-        [Required]
-        public required string ConfirmPassword { get; set; }
-    }
+    //     [Required]
+    //     public required string ConfirmPassword { get; set; }
+    // }
 }
