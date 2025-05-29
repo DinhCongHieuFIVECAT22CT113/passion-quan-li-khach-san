211bca7965f2abffe77b66348e60dcb77d6a8109
DinhCongHieu_FIVECAT_22CT113 <163360363+DinhCongHieuFIVECAT22CT113@users.noreply.github.com>
2025-05-29 18:29:17 +0700

    fix

--- backend/be-quanlikhachsanapi/obj/Debug/net8.0/be-quanlikhachsanapi.AssemblyInfo.cs
+++ backend/be-quanlikhachsanapi/obj/Debug/net8.0/be-quanlikhachsanapi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("be-quanlikhachsanapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0c5a70d84d9bfd2decc607effea127db42c0a00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76f30725df55fab88afd292895f7481106afba35")]
 [assembly: System.Reflection.AssemblyProductAttribute("be-quanlikhachsanapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("be-quanlikhachsanapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
