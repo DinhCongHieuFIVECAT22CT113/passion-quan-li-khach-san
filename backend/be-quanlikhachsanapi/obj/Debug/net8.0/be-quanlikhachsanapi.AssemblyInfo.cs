5376b89f1bac462d97be97e34a96623732ad7175
TruongTrungTin_FIVE_CAT <128278161+TruongTrungTinFIVECAT22CT113@users.noreply.github.com>
2025-06-07 00:53:05 +0700

    sửa màu chữ trong css

--- backend/be-quanlikhachsanapi/obj/Debug/net8.0/be-quanlikhachsanapi.AssemblyInfo.cs
+++ backend/be-quanlikhachsanapi/obj/Debug/net8.0/be-quanlikhachsanapi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("be-quanlikhachsanapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77e9bdc0c0203a5d0971f1b778674f52239d54a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c26b01f1a812d45755fd7922f5963a6ffd700223")]
 [assembly: System.Reflection.AssemblyProductAttribute("be-quanlikhachsanapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("be-quanlikhachsanapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
